5b6f18cf0e88f9f403338bcf5a3a0b9300c8c342
Colin Moore-Hill <colin.moore-hill@met.police.uk>
2016-05-06 15:40:26 +0100

    Adding Requirements file and vEnv

--- behaveRunner.sh
+++ behaveRunner.sh
@@ -1,5 +1,11 @@
-#!/usr/bin/env bash
-
-pip install behave
-
+#!/bin/bash
+apt-get install idle-python3
+if [ ! -d "pyEnvJenkins" ]; then
+        virtualenv pyEnvJenkins
+fi
+. pyEnvJenkins/bin/activate
+pip install -r requirements.txt
+cd BrowserStackBehave/
 behave --junit
+
+deactivate
