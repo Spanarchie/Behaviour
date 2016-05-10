4afae9c90339eae4d443809b201eaab35b5c915a
Colin Moore-Hill <colin.moore-hill@met.police.uk>
2016-05-10 17:11:24 +0100

    Removed creation of venv option.

--- behaveRunner2.sh
+++ behaveRunner2.sh
@@ -1,9 +1,5 @@
-#github_rsa!/bin/bash apt-get install idle-python3
-if [ ! -d "pyEnvJenkins" ]; then
-        virtualenv pyEnv2_7Jenkins
-        . pyEnv2_7Jenkins/bin/activate
-        pip install -r requirements.txt
-fi
+#!/bin/bash
+
 . pyEnv2_7Jenkins/bin/activate
 pip install -r requirements.txt
 
